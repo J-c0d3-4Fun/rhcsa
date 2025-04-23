9817dea64c2ee057dc72c050ad1760fde3a3947a
J-c0d3-4Fun <jabbrown001@gmail.com>
2025-04-23 08:38:46 -0400

    upgrading my gitcommit script

--- gitcommit.sh
+++ gitcommit.sh
@@ -3,14 +3,23 @@
 echo
 echo
 
-git add .
+echo "What would you like to add to github?"
+echo "Please provide the absolute or full path from the directory your are currently in."
+read folderPath
+if [[ -z "$folderPath"  ]]; then
+	echo "This is not a valid folder path"
+	exit 1
+else
+	git add "$folderPath"
+fi 
 
 echo "What message do you to add to the commit?"
 read message
 if [[ $? -eq 0 ]]; then
-        git commit -am "$message"
-        git branch --show-current
-        git push $1
-else
-        echo "error occurred"
+	git commit -am "$message"
+	git branch --show-current 
+	git push $1
+else 
+	echo "error occurred"
 fi
+
